5dc1ebede980834d080b72a69c3e28c7fb973fb5
Russ Parmer <russell.parmer@weave.works>
2022-08-01 10:45:28 -0700

    testing full crd

--- api/v1alpha2/release_types.go
+++ api/v1alpha2/release_types.go
@@ -1,16 +1,7 @@
 package v1alpha2
 
 type ReleaseReference struct {
-	// Kind of the referent.
 	// +kubebuilder:validation:Enum=Kustomization;HelmRelease
-	// +required
 	Kind string `json:"kind"`
-
-	// Name of the referent.
-	// +required
 	Name string `json:"name"`
-
-	// Namespace of the referent, defaults to the namespace of the Kubernetes resource object that contains the reference.
-	// +optional
-	// Namespace string `json:"namespace,omitempty"`
 }
